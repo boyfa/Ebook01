9b32de89932381e23c1d54d48183f38a5e743203
Vu Tan Nguyen <procfvu2012@gmail.com>
2019-09-11 21:32:19 +0700

    initialize project

---
+++ app/src/main/java/com/example/test/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //start main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
